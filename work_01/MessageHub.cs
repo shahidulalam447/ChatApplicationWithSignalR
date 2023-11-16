9b3168e8bba5bff27f3e22a558f143a57cb225dd
shahidulalam447 <shahidulalam447@gmail.com>
2023-11-16 15:11:24 +0600

    Chat Application With SignalR Technology

---
+++ work_01/MessageHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace work_01
+{
+    public class MessageHub : Hub
+    {
+        private static Dictionary<string, string> users = new Dictionary<string, string>();
+        public void addMe(string name)
+        {
+            users.Add(Context.ConnectionId, name);
+            Clients.All.updateUsers(users.Values.ToArray());
+        }
+        public void Send(string name, string msg)
+        {
+            Clients.All.broadcast(name,msg);
+        }
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            users.Remove(Context.ConnectionId);
+            Clients.All.updateUsers(users.Values.ToArray());
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ work_01/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(work_01.Startup))]
+
+namespace work_01
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
